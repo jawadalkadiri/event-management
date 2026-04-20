86033744a9ec4b0aef7920b243d2e0099bdb834f
JawadKadery <jawadalkadiri@gmail.com>
2026-04-20 08:02:58 +0300

    feat(event): implement event creation API with validation

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -14,7 +14,7 @@ class EventController extends Controller
      */
     public function index()
     {
-        return User::all();
+        return Event::all();
     }
 
     /**
@@ -22,7 +22,19 @@ class EventController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+
+        $event = Event::create([
+            ...$request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'start_time' => 'required|date',
+            'end_time' => 'required|date|after:start_time'
+            ]),
+            'user_id' => 1
+        ]);
+
+        return $event;
     }
 
     /**
